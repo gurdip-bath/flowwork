bd3c185dc5f29df9b59f4003d513f08bed247779
Gurdip Bath <bathgurdip1@gmail.com>
2025-03-17 21:37:38 +0000

    implement get_user database query function

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -2,3 +2,6 @@ from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
 from app.core.security import get_password_hash
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()

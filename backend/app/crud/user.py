4701a555648072552aa8ee00f128384ec6b33bbc
Gurdip Bath <bathgurdip1@gmail.com>
2025-03-18 20:25:01 +0000

    add get_user_by_email lookup function

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -5,3 +5,6 @@ from app.core.security import get_password_hash
 
 def get_user(db: Session, user_id: int):
     return db.query(User).filter(User.id == user_id).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()

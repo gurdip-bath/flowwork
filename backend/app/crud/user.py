d982b835ec87778bac02c26b0539a6f7b213009d
Gurdip Bath <bathgurdip1@gmail.com>
2025-03-20 19:26:18 +0000

    implement create_user with password hashing

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -11,3 +11,16 @@ def get_user_by_email(db: Session, email: str):
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(User).offset(skip).limit(limit).all()
+
+def create_user(db: Session, user: UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user = User(
+        email=user.email,
+        password_hash=hashed_password,
+        role=user.role,
+        is_active=user.is_active
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user

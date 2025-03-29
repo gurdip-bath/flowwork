fbc8d0406ca6d97fc628915e62676b07bfd89d65
Gurdip Bath <bathgurdip1@gmail.com>
2025-03-29 13:10:39 +0000

    feat: add departmentbase import

--- backend/app/routers/department.py
+++ backend/app/routers/department.py
@@ -4,7 +4,7 @@ from typing import List
 
 from app.dependencies import get_db, get_current_active_user, get_current_hr_user, get_current_admin_user
 from app.crud import department as department_crud
-from app.schemas.department import DepartmentResponse, DepartmentBase
+from app.schemas.department import DepartmentResponse, DepartmentUpdate, DepartmentBase
 
 router = APIRouter(
     prefix="/departments",

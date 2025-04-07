462cdf8434b7d68609e5f53e1d740eb768fcd757
Gurdip Bath <bathgurdip1@gmail.com>
2025-04-07 20:52:02 +0100

    refactor(styles): migrate from CSS to Tailwind utility classes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import LoginPage from './pages/Login'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <>
+    <div className ="w-full min-h screen">
       <LoginPage />
+    </div>
     </>
   )
 }

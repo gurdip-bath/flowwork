1f49ac82c75a4e5a2b865f4dc7cfea5fb32a7743
Gurdip Bath <bathgurdip1@gmail.com>
2025-04-22 19:14:59 +0100

    feat: create basic AppHeader component with welcome message

--- frontend/src/components/AppHeader/AppHeader.tsx
+++ frontend/src/components/AppHeader/AppHeader.tsx
@@ -1,3 +1,12 @@
 import React from 'react';
 import AuthContext from "../../context/AuthContext";
 
+// CURRENT TO DO - use React doc to create the app header component. style it with tailwind and add the logo.
+
+function AppHeader () {
+    return (
+        <div>
+            <h1 className='bold'>Welcome Mikey</h1>
+        </div>
+    )
+}

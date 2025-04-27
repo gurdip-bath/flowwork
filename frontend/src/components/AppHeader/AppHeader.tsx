fe1f283a3aab1c466153fbf265324fac05d2456e
Gurdip Bath <bathgurdip1@gmail.com>
2025-04-27 17:15:36 +0100

    style: add basic styling to AppHeader component

--- frontend/src/components/AppHeader/AppHeader.tsx
+++ frontend/src/components/AppHeader/AppHeader.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const AppHeader: React.FC = () => { 
-  return (
-    <header>
-      <h1>FlowWork HR</h1>
+   return (
+    <header className="bg-white shadow-md px-6 py-4">
+      <div className="container mx-auto">
+        <h1 className="text-xl font-bold text-blue-600">FlowWork HR</h1>
+      </div>
     </header>
   );
 };

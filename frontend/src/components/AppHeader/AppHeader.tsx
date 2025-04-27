177960250f26bdc17d1fbdc74e0406db2464eb95
Gurdip Bath <bathgurdip1@gmail.com>
2025-04-27 17:06:52 +0100

    refactor: simplify AppHeader to minimal header with title

--- frontend/src/components/AppHeader/AppHeader.tsx
+++ frontend/src/components/AppHeader/AppHeader.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 const AppHeader: React.FC = () => { 
   return (
-      <div className="flex">
-        <div className="w-64 bg-gray-100">Sidebar</div>
-        <div className="flex-1">Main content</div>
-      </div>
-    );
-  }
-  
-  export default AppHeader;
+    <header>
+      <h1>FlowWork HR</h1>
+    </header>
+  );
+};
+
+export default AppHeader;

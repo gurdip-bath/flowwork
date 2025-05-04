ee275efe8991b4deeb8a569298688a747b46b4cb
Gurdip Bath <bathgurdip1@gmail.com>
2025-05-04 10:25:54 +0100

    feat: implement basic routing structure with dashboard and login routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
 // commenting out import for now 
 // import { useState } from 'react'
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import AppHeader from './components/AppHeader/AppHeader'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
-    <>
-    <div className ="w-full min-h screen">
+    <div className="w-full min-h-screen">
       <AppHeader/>
-      <LoginPage />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+      </Routes>
     </div>
-    </>
   )
 }
 

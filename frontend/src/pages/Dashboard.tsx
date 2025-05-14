cf83fa9e231269a2a3cdd79f7f187fb14f4c9d51
Gurdip Bath <bathgurdip1@gmail.com>
2025-05-14 19:58:20 +0100

    feat: integrate TaskCard component with sample data

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import TaskCard from '../components/Taskcard/TaskCard';
 
 
 const Dashboard = () => {
@@ -7,6 +8,12 @@ const Dashboard = () => {
         <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
             <h1 className="text-3xl font-bold text-gray-800">Welcome to the Dashboard</h1>
             <p className="text-gray-600">This is where you can manage your tasks and projects.</p>
+            <TaskCard
+                name = "task1"
+                id = "1"
+                description = " the first task"
+                onButtonClick = {() => console.log('Button clicked')}
+            />
         </div>
     )
 }

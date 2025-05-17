f6a29cded8ce15af11a2fd1e0ced39c8279109ba
Gurdip Bath <bathgurdip1@gmail.com>
2025-05-17 11:30:28 +0100

    feat: create basic sidebar component

---
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function SideBar () {
+    return ( 
+        <div>
+            <h1>this is the sidebar</h1>
+            <p>this is the sidebar content</p>
+        </div>
+    )
+}

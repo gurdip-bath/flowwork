b0833598423efb196d03737b60a9f0dc03a6ccc8
Gurdip Bath <bathgurdip1@gmail.com>
2025-05-18 19:13:39 +0100

    style: styled sidebar

--- frontend/src/components/SideBar/SideBar.tsx
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 
 function SideBar () {
     return ( 
-        <div className='fixed h-screen flex flex-col'>
+        <div className='fixed top-16 left-0 w-30 m-0 h-screen flex flex-col bg-secondary text-white shadow-lg pt'>
             <ul>
-                <li className="bg-gray-400 text-amber-300 shadow-1g">Dashboard</li>
-                <li className="bg-gray-400 text-amber-300 shadow-1g">Tasks</li>
-                <li className="bg-gray-400 text-amber-300 shadow-1g">Projects</li>
+                <li className="bg-secondary/90 border-l-4 border-primary p-4 my-2 rounded-r">Dashboard</li>
+                <li className="bg-secondary/90 hover:border-l-4 hover:border-primary p-4 my-2 rounded-r">Tasks</li>
+                <li className="bg-secondary/90 hover:border-l-4 hover:border-primary p-4 my-2 rounded-r">Projects</li>
             </ul>
         </div>
     )
